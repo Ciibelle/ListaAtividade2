12678b32ac5333c15125f500c36c31fc902572cc
Ciibelle <cibellegon6565@gmail.com>
2022-05-24 20:50:17 -0300

    Commit Inicial

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Atividade2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double euro = 5.13;
+            double real = 0;
+            double qt = 750;
+
+            real = (qt * euro);
+            Console.WriteLine($"O valor em real é: {real}");
+        }
+    }
+}
